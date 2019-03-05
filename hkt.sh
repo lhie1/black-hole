abfeaf6d6b3ddfdcebbc4f69cfb1b181b596207b
lhie1 <lhie1x@gmail.com>
2019-03-05 14:30:45 +0800

    Update hkt.sh

--- hkt.sh
+++ hkt.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+echo "                            "
+echo " Reset PPPOe Connection !!! "
+echo "                            "
 chattr -i /etc/resolv.conf
 dhclient -r && \
 rm -rf /var/lib/dhcp/* && \
