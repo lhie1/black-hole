da50ffabdd73034817c24ab6e4d47a6af6426a69
lhie1 <lhie1x@gmail.com>
2019-03-19 02:07:05 +0800

    Update hkt.sh

--- hkt.sh
+++ hkt.sh
@@ -3,7 +3,7 @@ echo "                            "
 echo "       Renew DHCP !!!       "
 echo "                            "
 chattr -i /etc/resolv.conf
-dhclient -r  eth0 && \
+dhclient -r eth0 && \
 rm -rf /var/lib/dhcp/*
 sleep 1
 dhclient -v eth0
