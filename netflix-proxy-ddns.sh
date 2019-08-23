148469b79980346697b2325641a5c68955fab967
lhie1 <lhie1x@gmail.com>
2019-08-23 19:10:04 +0800

    Create netflix-proxy-ddns.sh

---
+++ netflix-proxy-ddns.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ ! -f "/tmp/current_ip.txt" ]
+then
+    CURRENT_IP=`curl -s http://ipv4.ip.sb`
+    echo $CURRENT_IP > /tmp/current_ip.txt
+fi
+NEW_IP=`curl -s http://ipv4.ip.sb`
+CURRENT_IP=`cat /tmp/current_ip.txt`
+if [ "$NEW_IP" = "$CURRENT_IP" ]
+then
+echo "No Change in IP Adddress"
+else
+sed -i "s/$CURRENT_IP/$NEW_IP/g" /root/netflix-proxy/dnsmasq.conf
+docker restart dnsmasq
+echo $NEW_IP > /tmp/current_ip.txt
+fi
