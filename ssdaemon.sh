71a7664a87255ff941b5f68e9c709be16c6e8cfe
lhie1 <lhie1x@gmail.com>
2019-11-16 01:00:40 +0800

    Update

---
+++ ssdaemon.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+while :
+do
+	for i in `netstat -tlnp |grep -E "152|153|156|21111|21112|21113|873|1099" |awk '$2 >128 {print $7}' |cut -d"/" -f1 |awk '!a[$0]++'`
+	do
+		for j in `docker inspect -f "{{.Id}} {{.State.Pid}}"  $(docker ps -q)|grep $i |awk '{print $1}'`
+		do 
+			docker restart $j && echo `date` >>/root/ssdaemon.log
+		done
+	done
+	sleep 5
+done
